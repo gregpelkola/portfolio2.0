602b59c4cbfc2b3fa5d61817b6abaf8b7ff7198b
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-02 17:56:29 -0600

    removed unused import for link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Navigation from './Navigation';
 import { Container, Row, Col } from 'react-bootstrap';
 import "../style/Header.css";
@@ -17,6 +16,6 @@ function Header() {
             <Navigation />
         </>
     );
-}
+};
 
 export default Header;
