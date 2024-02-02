413d48585f9f9c375dfbdb6010d1f9dde7bcee08
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-01 18:09:45 -0600

    set up react, bootstrap imports

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style/App.css";
