7b81b75ea4ea8d8517723fbcc9d92a8c8d1378f7
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-01 18:17:25 -0600

    created app function with routes

--- src/App.js
+++ src/App.js
@@ -9,3 +9,23 @@ import Footer from './components/Footer';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style/App.css";
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+            <Header />
+            <div className="container mt-5">
+                <Routes>
+                    <Route path="/" element={<About />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/resume" element={<Resume />} />
+                </Routes>
+            </div>
+            <Footer />
+        </Router>
+    </div>
+  );
+}
+export default App;
