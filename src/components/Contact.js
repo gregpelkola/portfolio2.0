8b53da01f6694285158cef1bfbb51fec56b63afb
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-02 16:47:50 -0600

    set initial state for name, email, message and form errors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,12 @@
 import React, {useState} from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import "../style/Contact.css";
+
+function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [nameError, setNameError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [messageError, setMessageError] = useState(false);
+    const [messageSent, setMessageSent] = useState(false);
