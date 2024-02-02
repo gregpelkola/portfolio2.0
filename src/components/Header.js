079f9fe4f4a227e03b9a44cb74d89c337eb7d185
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-02 17:37:49 -0600

    header.js

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navigation from './Navigation';
+import { Container, Row, Col } from 'react-bootstrap';
+import "../style/Header.css";
+
+function Header() {
+    return (
+        <>
+            <Container fluid className="bg-dark">
+                <Row className='justify-content-center text-center mb=0'>
+                    <Col className='pb-o'>
+                        <h1 className='text-white' style={{fontSize: '3rem'}}>Greg Pelkola</h1>
+                    </Col>
+                </Row>
+            </Container>
+            <Navigation />
+        </>
+    );
+}
+
+export default Header;
