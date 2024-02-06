51ea462f6cd7f3b3dcfd3c5d6e3dcfe974bf9678
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-05 18:36:30 -0600

    fixed header not displaying across whole page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,7 +6,7 @@ function Project(props) {
         <div className='card'>
             <h5 className='card-title'>{props.title}</h5>
             <a href={props.deployed} target='_blank' rel='noreferrer'>
-                {/* <img src={props.imgSrc} className='card-img-top' alt={props.imgAlt} /> */}
+                <img src={props.imgSrc} className='card-img-top' alt={props.imgAlt} />
             </a>
             <div className='card-body'>
                 <p className='card-text'>{props.description}</p>
